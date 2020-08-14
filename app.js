840117367027c29395c28d72ae22125effd87a5b
Felipe Soto C <pipesoto13@gmail.com>
2020-08-13 23:32:37 -0500

    -get hour and minute of message sent

--- app.js
+++ app.js
@@ -88,8 +88,12 @@ $btnSend.on('click', function (e) {
   const value = $inputMessage.val();
 
   if (value !== '') {
+    const timeNow = new Date();
+    const timeHour = timeNow.getHours();
+    const timeMinute = timeNow.getMinutes();
+
     const message =  {
-      time: '19:45',
+      time: `${timeHour}:${timeMinute}`,
       content: value,
       author: {
         name: "Branson Spencer",
